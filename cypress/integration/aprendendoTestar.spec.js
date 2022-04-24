c50976782f8d01988da06cbd01232a9e3dd4baa0
Natalia Amancio Belo de Oliveira <CIANDT\nataliabelo@lnb028664bhz.ciandt.global>
2022-04-24 14:09:45 -0300

    Deletar usuário

--- cypress/integration/aprendendoTestar.spec.js
+++ cypress/integration/aprendendoTestar.spec.js
@@ -2,14 +2,24 @@
 
 context('Aprendendo a testar', () => {
 
-    it('Cadastrar usuário', () => {
+    beforeEach(() => {
 
         cy.visit('http://www.aprendendotestar.com.br/treinar-automacao.php')
+        
+    });
+
+    it('Cadastrar usuário', () => {
 
         cy.get('[name=form_usuario]').type('NBelo')
         cy.get('[name=form_senha]').type('654321')
         cy.get('[name=form_nome]').type('Nat')
         cy.get('input[type="submit"]').click()
+        
+    });
+
+    it.only('Deletar usuário', () => {
+
+        cy.get('tr:nth-child(5) > td:nth-child(5) > a').click()
         
     });
     
